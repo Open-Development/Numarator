773edffa23cac363747ee0330c7011be53d893ea
Sevcan ALKAN <sevcanalkan@outlook.com.tr>
2019-10-13 17:52:55 +0300

    #2 degiskenler olusturuldu

--- Numarator/Main.cs
+++ Numarator/Main.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace Numarator
 {
     public partial class Main : Form
     {
+        private Hashtable Masalar;
+        private Queue<int> Bekleyenler;
+
+
         public Main()
         {
             InitializeComponent();
@@ -22,11 +27,36 @@ namespace Numarator
             this.Text = "Numarator";
             btnGetNumber.Text = "Get Number";
             lblLastNumber.Text = "-";
+
+            this.Masalar = new Hashtable();
+            this.Bekleyenler = new Queue<int>();
+
+            this.masalariYukle();
+        }
+
+        private void masalariYukle()
+        {
+            this.Masalar.Add(1, 0);
+            this.Masalar.Add(2, 0);
+            this.Masalar.Add(3, 0);
+            this.Masalar.Add(4, 0);
+            this.Masalar.Add(5, 0);
+            this.Masalar.Add(6, 0);
         }
 
         private void BtnGetNumber_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void numaraEkle(int numara)
+        {
+            this.Bekleyenler.Enqueue(numara);
+        }
+
+        private int siradaki()
+        {
+            return this.Bekleyenler.Dequeue();
         }
     }
 }
